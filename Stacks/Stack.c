b1be6b2253afe8d1657a3d3fc3b8bde975378dda
Joel Nickson <joel2000nickson@gmail.com>
2021-10-30 00:42:08 +0530

    shortened the spacings

--- Stacks/Stack.c
+++ Stacks/Stack.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 int a[200], top = -1, max_size, item, e;
-void push()
-{
-	if(top >= max_size - 1)		
-	{printf("OVERFLOW!");}
-	else{
+void push() {
+	if(top >= max_size - 1)	
+		printf("OVERFLOW!");
+	else {
 		++top;
 		printf("Enter the element to be pushed:");
 		scanf("%d", &e);
@@ -13,26 +12,21 @@ void push()
 		printf("\n%d has been pushed into the stack.", e);
 	}	
 }
-void pop()
-{
+void pop() {
 	if(top < 0)			
-	printf("UNDERFLOW!");
-	else{
+		printf("UNDERFLOW!");
+	else {
 		item = a[top];
 		--top;
 		printf("\n%d has been popped from the stack.", item);
 	}	
 }
-void display()
-{
+void display() {
 	printf("The stack now is:\n");
 	for(int i = top; i >= 0; i--)
-	{
 		printf("\n%d", a[i]);
-	}	
 }
-void main()
-{
+void main() {
 	int ch, choice = 1, a[200];
 	printf("Enter maximum size of stack:");
 	scanf("%d", &max_size);
@@ -40,14 +34,10 @@ void main()
 		printf("\nChoose your option:\n1.Push\n2.Pop\n3.Display stack\n4.Exit\nEnter your option:");
 		scanf("%d", & ch);
 		switch(ch){
-			case 1: push();
-				break;
-			case 2: pop();
-				break;
-			case 3: display();
-				break;
-			case 4: exit(0);
-				break;
+			case 1: push();	  break;
+			case 2: pop();	  break;
+			case 3: display();break;
+			case 4: exit(0);  break;
 			default: printf("WRONG OPTION!");
 		}
 		printf("\n\nWant to perform more operations (1/0)?:");
